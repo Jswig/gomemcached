a7a524bfd6fac7a7eda9ea645c0fd0259242670a
anders.poirel <contact@anderspoirel.net>
2025-07-08 10:06:47 -0700

    utility for zero time

--- internal/util/time.go
+++ internal/util/time.go
@@ -2,6 +2,13 @@ package util
 
 import "time"
 
+// returns the current time in the UTC timezone
 func NowUTC() time.Time {
 	return time.Now().UTC()
+}
+
+// returns the "zero" time (0001-01-01 00:00). This is a placeholder for an unitialized
+// time
+func ZeroTime() time.Time {
+	return time.Time{}
 }
