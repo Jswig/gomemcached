f45fab726eb1b3cf2d246a599fde3cb6f428ac1e
Anders Poirel <contact@anderspoirel.net>
2025-08-30 19:22:51 -0700

    start implementing add

--- internal/parser.go
+++ internal/parser.go
@@ -3,33 +3,63 @@ package internal
 import (
 	"bytes"
 	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/Jswig/gomemcached/internal/util"
 )
+
+const expirationUnixTimestampThreshold = 60 * 60 * 24 * 30
+
+// threshold after which the expiration is treated as a Unix time in seconds rather
+// than an offset in seconds from the current time
 
 func ParseCommand(input []byte) (Command, error) {
 	input = bytes.TrimSuffix(input, []byte("\r\n"))
+	inputLines := bytes.Split([]byte("\r\n"))
+	commandLine := string(inputLines[0])
 	// TODO: make this work with other ASCII whitespace characters
-	name, arguments, found := bytes.Cut(input, []byte(" "))
-	if !found {
-		return nil, fmt.Errorf("no command provived in input: %v", string(input))
+	commandItems := strings.Split(commandLine, " ")
+	if len(commandItems) <= 2 {
+		return nil, fmt.Errorf("no command provided in input: %s", commandLine)
 	}
-	cmdName := string(name)
+	commandName := commandItems[0]
 
 	var cmd Command
-	switch cmdName {
+	switch commandName {
 	case "add":
-		cmd = &Add{}
-	case "delete":
-		cmd = &Delete{}
-	case "get":
-		// TODO: change get to work on byte keys instead of atrings to avoid extra
-		// conversions
-		rawKeys := bytes.Split(arguments, []byte(" "))
-		if len(rawKeys) != 1 {
-			return nil, fmt.Errorf("must provide at least 1 key for 'get' command")
+		arguments := commandItems[1:]
+		if len(arguments) < 4 {
+			return nil, fmt.Errorf("invalid 'add' command: %s", commandLine)
 		}
-		keys := make([]string, 0, len(rawKeys))
-		for _, key := range rawKeys[1:] {
-			keys = append(keys, string(key))
+		if len(inputLines) != 2 {
+			return nil, fmt.Errorf("missing data block for 'add' command")
+		}
+		dataBlock := inputLines[1]
+		key := arguments[0]
+		exptime, err := strconv.Atoi(arguments[2])
+		if err != nil {
+			return nil, fmt.Errorf("invalid expiration time", exptime)
+		}
+		var expiresAt time.Time
+		if exptime <= expirationUnixTimestampThreshold {
+			expiresAt = util.NowUTC().Add(time.Duration(exptime) * time.Second)
+		} else {
+			expiresAt = time.Unix(int64(exptime), 0)
+		}
+		cmd = &Add{
+			key:       key,
+			value:     dataBlock,
+			expiresAt: expiresAt,
+		}
+	case "delete":
+		key := commandItems[1]
+		cmd = &Delete{key}
+	case "get":
+		keys := commandItems[1:]
+		if len(keys) != 1 {
+			return nil, fmt.Errorf("must provide at least 1 key for 'get' command")
 		}
 		return &Get{keys}, nil
 	case "replace":
@@ -37,7 +67,11 @@ func ParseCommand(input []byte) (Command, error) {
 	case "set":
 		cmd = &Set{}
 	default:
-		return nil, fmt.Errorf("unknown command '%s'", cmdName)
+		return nil, fmt.Errorf("unknown command: '%s'", commandName)
 	}
 	return cmd, nil
+}
+
+func parseStorageCommandElements(arguments []string) (key string, expiresAt time.Time, err error) {
+	return
 }
